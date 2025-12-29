a98ce22a0e72e4de0bb8abe90e3fdf02600995b4
Juan Flores <74993575+jc30flores@users.noreply.github.com>
2025-12-29 12:35:19 -0600

    Add core index rename merge migrations

---
+++ backend/apps/core/migrations/0003_merge_0002_taxconfig_update_0002_rename_core_branch_code_1f6f10_idx_core_branch_code_b50af7_idx_and_more.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("core", "0002_taxconfig_update"),
+        ("core", "0002_rename_core_branch_code_1f6f10_idx_core_branch_code_b50af7_idx_and_more"),
+    ]
+
+    operations = []
