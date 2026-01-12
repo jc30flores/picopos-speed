45e87e14b17e3fff0dba25708e124d284ab1cfe4
Juan Flores <74993575+jc30flores@users.noreply.github.com>
2026-01-12 00:53:00 -0600

    Allow CSRF from Caddy localhost

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -126,6 +126,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8182",
     "http://127.0.0.1:8182",
+    "http://localhost:9102",
     "https://pico-de-gallo-pos.cuskatech.com",
 ]
 
